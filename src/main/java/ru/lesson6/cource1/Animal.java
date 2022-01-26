7c10c1fae97b05d0ba176ec28c517f5502dcb298
geekbrains <info@geekbrains.ru>
2022-01-26 22:20:00 +0300

    DZ 6 urokл

---
+++ src/main/java/ru/lesson6/cource1/Animal.java
@@ -0,0 +1,13 @@
+package ru.lesson6.cource1;
+
+public class Animal {
+    void run() {
+        String a = "Prepyatstvie_100_meter";
+        System.out.println(a + " " + "Animal ran 300 meter");
+    }
+    void swim() {
+        String b = "Prepyatstvie_20_meter";
+        System.out.println(b + " " + "Animal ran 300 meter");
+    }
+
+}
