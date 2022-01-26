5901e8722201ba2ceb378eb68a2eb91da038d4c5
geekbrains <info@geekbrains.ru>
2022-01-26 17:22:35 +0300

    DZ 6 urok

---
+++ src/main/java/ru/lesson6666/oop/Animal.java
@@ -0,0 +1,20 @@
+package ru.lesson6666.oop;
+
+public class Animal {
+
+
+    void run() {
+        String a = "Prepyatstvie_100_meter";
+        System.out.println(a + " " + "Animal ran 300 meter");
+    }
+    void swim() {
+        String b = "Prepyatstvie_20_meter";
+        System.out.println(b + " " + "Animal ran 300 meter");
+    }
+
+}
+
+
+
+
+
---
+++ src/main/java/ru/lesson6666/oop/Dog.java
@@ -0,0 +1,19 @@
+package ru.lesson6666.oop;
+
+    public class Dog extends Animal{
+        void run() {
+            int distance = 400;
+            if (distance <= 400) {
+                System.out.println("Dog successfully ran your distance");
+            } else {
+                System.out.println("Dog can not run more than 500 meter");
+            }   }
+            void swim() {
+            int distance = 20;
+            if (distance>=10) {
+                System.out.println("Dog swims not more than 10 meter");
+            }  else {
+                System.out.println("Dog successfully swam your distance");
+            }}}
+
+
